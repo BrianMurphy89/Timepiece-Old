0b79301b0c0d0b92b6e83c8cfa3b277a084dbafc
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-05-03 16:03:57 -0400

    Database created: Timepiece and table: users created.

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const {Client} = require('pg');
+const format = require('pg-format')
+const port = 3000;
 
-app.listen(3000, ()=>{
-    console.log('Mongod is listening');
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}))
+
+const homepageController = require('./controllers/homepage.js');
+app.use('/', homepageController)
+
+const client = new Client({
+    user:'BrianJR',
+    host:'http://localhost',
+    database:'timepiece',
+    port:5432,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err,res) => {
+    console.log(err,res)
+    client.end()
+})
+
+
+app.listen(port, ()=>{
+    console.log('listening');
 })
